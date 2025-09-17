85c93d3212ce73cec3a321187cacb7567fb3cebf
JJ07701 <jangraj845@gmail.com>
2025-09-17 14:51:24 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("The size of int is:%zu\n",sizeof(int));
+printf("The size of float is :%zu\n",sizeof(float));
+printf("The size of char is:%zu\n",sizeof(char));
+printf("The size of double is:%zu\n",sizeof(double));
+printf("The size of long long is:%zu\n",sizeof(long long));
+return 0;
+}
