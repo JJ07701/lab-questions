e62b8cfb52c423c986424b3fddd97e5ff5a982dc
JJ07701 <jangraj845@gmail.com>
2025-09-17 14:52:04 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+float result;
+printf("Enter two number:");
+scanf("%d%d",&a,&b);
+result=(float)a/b;
+printf("The division of %d by %d is:%.2f\n",a,b,result);
+return 0;
+}
